8b6cb9c5ef1e75be830d3796a0e6fb3a521244c3
mokshita711 <mokshitseshu711@gmail.com>
2025-03-16 23:55:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputBox = document.getElementById("input-box")
+const listcontainer = document.getElementById("list-container")
+function addTask() {
+    if (inputBox.value == '') {
+        alert("You must write something");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listcontainer.append(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+    }
+    inputBox.value = " ";
+}
